faeeeba191f52c8a5bfa60dea8b0defda9ddcb40
Hanzcode <220441100156@student.trunojoyo.ac.id>
2025-10-01 18:10:14 +0700

    test

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET' && realpath(__FILE__) == realpath($_SERV
     die(header('location: ../index.php'));
 }
 
-$host = 'localhost';
+$host = '2raEEvCp1AcJy3cFu6n0ozE808v7kGFQgTEB8YSUtAUc0Rlomce5N8XTGfcxm1kV@kwcggwkso48ocsks8880g40k';
 $db = 'myirt_adaptive_learning';
 $user = 'root';
 $pass = 'KZpcshK2nKeG47DYZfHvC2tbehurKwx2AGzoj6fFpAoWoX1Gl43i4B9jWQhsJ0Oe';
